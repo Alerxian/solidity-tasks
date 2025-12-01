b7f15f39eecc8c527b1ddf2cef00043a7f9f08d0
OlgaAlerx <1967376524@qq.com>
2025-12-01 15:11:44 +0800

    feat: 部署到sepolia测试网

--- 3-3stake/web/src/lib/wagmi.tsx
+++ 3-3stake/web/src/lib/wagmi.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { createConfig, http, cookieStorage, createStorage } from "wagmi";
 import { injected, walletConnect } from "@wagmi/connectors";
+import { sepolia } from "wagmi/chains";
 import type { Chain } from "viem";
 
 const localChain: Chain = {
   id: 31337,
   name: "Localhost",
   nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-  rpcUrls: { default: { http: [process.env.NEXT_PUBLIC_RPC_URL || "http://localhost:8545"] } },
+  rpcUrls: {
+    default: {
+      http: [process.env.NEXT_PUBLIC_RPC_URL || "http://localhost:8545"],
+    },
+  },
 } as const;
 
 export const config = createConfig({
-  chains: [localChain],
+  chains: [localChain, sepolia],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
@@ -30,5 +35,6 @@ export const config = createConfig({
     [localChain.id]: http(
       process.env.NEXT_PUBLIC_RPC_URL || "http://localhost:8545"
     ),
+    [sepolia.id]: http(),
   },
 });
